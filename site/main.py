a629f7190cb6abcb7516b074ebef0e4bb1466832
Craig Sexauer <csexauer@fb.com>
2019-08-02 16:25:24 -0700

    readme

--- site/main.py
+++ site/main.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import request
-
+from datetime import date
+import mysql.connector
+return searchword
 
 app = Flask(__name__)
 
@@ -8,7 +10,36 @@ app = Flask(__name__)
 def hello_world():
     return ':)'
 
-@app.route('/request', methods=['GET'])
+@app.route('/requestfollowdata', methods=['GET'])
 def send_data():
-    searchword = request.args.get('key', '')
-    return searchword
+    ds = request.args.get('ds', '')
+    if ds == '':
+        ds = date.today()
+
+    cnx = mysql.connector.connect(user='csfb', password='beepboop',
+                          host='csfb.mysql.pythonanywhere-services.com',
+                          database='csfb$scrapes')
+
+    cursor = cnx.cursor()
+
+    cursor.execute("select * from followers_7d")
+
+    out = cursor.fetchall()
+
+    header = [(
+        'row', 
+        'name', 
+        'watch_hours', 
+        'broadcast_hours', 
+        'peak_viewers', 
+        'avg_viewers', 
+        'followers', 
+        'new_followers', 
+        'partnered', 
+        'mature', 
+        'language')]
+
+
+    
+    return header+out
+
